7238ed173f447f39b0cf2e0e57d64d4165ca9589
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-05 21:31:57 +0900

    Add files via upload

---
+++ 4.Task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHealt = 4;
+            int maxHealth = 10;
+            Console.CursorVisible = false;
+            DrawBar(playerHealt,maxHealth,15);
+        }
+
+        static void DrawBar(int value, int maxValue, int position, char symbolHealth = '#', char symbolEmptyHealte = '_')
+        {
+            string bar = "";
+
+            for (int i = 0; i < value; i++)
+            {
+                bar += symbolHealth;
+            }
+
+            Console.SetCursorPosition(0, position);
+            Console.Write("[");
+            Console.Write(bar);
+            bar = "";
+
+            for (int i = value; i < maxValue; i++)
+            {
+                bar += symbolEmptyHealte;
+            }
+
+            Console.Write(bar);
+            Console.Write("]");
+            Console.ReadKey();
+        }
+    }
+}
