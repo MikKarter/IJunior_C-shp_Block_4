0350652fe9019c85aac514a0bdb70c033e76e645
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-05 21:55:14 +0900

    Add files via upload

---
+++ 4.Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            bool isWork = true;
+
+            while (isWork)
+            {
+                string userInput = Console.ReadLine();
+                StringParce(userInput,ref isWork);
+            }
+        }
+
+        static void StringParce (string inputText, ref bool value)
+        {
+            int.TryParse(inputText, out int resultParse);
+            if (resultParse == 0)
+            {
+                Console.WriteLine("Input error. Please, input any number:");
+            }
+            else
+            {
+                Console.WriteLine(resultParse);
+                value = false;
+            }
+        }
+    }
+}
