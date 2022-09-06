6c523c14f3a8be8f73ea62894e4fd7f2e7974d2b
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-06 21:41:01 +0900

    Add files via upload

---
+++ 4.Task_5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mainArray = CreatingNewArray();
+            WriteArray(mainArray);
+            Shuffle(ref mainArray);
+            WriteArray(mainArray);
+        }
+
+        static int[] CreatingNewArray()
+        {
+            int lowerNumber = 0;
+            int upperNumber = 20;
+            Random tempNumber = new Random();
+            int[] tempArray = new int[upperNumber];
+
+            for (int i = 0; i < tempArray.GetLength(0); i++)
+            {
+                tempArray[i] = tempNumber.Next(lowerNumber, upperNumber);
+            }
+
+            return tempArray;
+        }
+
+        static int[] Shuffle(ref int[] mainArray)
+        {
+            Random tempNumber = new Random();           
+            
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                int tempRandomi = tempNumber.Next(0, mainArray.Length);
+                int tempValue;
+                tempValue = mainArray[i];
+                mainArray[i]=mainArray[tempRandomi];
+                mainArray[tempRandomi] = tempValue;
+            }
+
+            return mainArray;
+        }
+
+        static void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
